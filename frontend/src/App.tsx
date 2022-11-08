73e66a2c118706e5caf29c779680cf0ac0a1b725
akrdevtech <akhilkrishnan.freelancer@gmail.com>
2022-11-08 11:35:44 +0530

    desktop app root setup - complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import APIs from './api';
 
 function App() {
+  const getApiCall = () => {
+    APIs.studentAPIs.fetchUserData().then(res => {
+      console.log(res);
+    })
+  }
+
   return (
-    <><h1>Hello</h1></>
+    <><h1>Hello</h1><button onClick={getApiCall}>Trigger</button></>
   )
 }
 
