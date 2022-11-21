63818cc1a907c308bdda1f843915118dd9e34757
akrdevtech <akhilkrishnan.freelancer@gmail.com>
2022-11-21 15:31:40 +0530

    dist dir removed

--- backend/dist/lib/enums/users.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserTypes = void 0;
-var UserTypes;
-(function (UserTypes) {
-    UserTypes["ADMIN"] = "admin";
-    UserTypes["STUDENT"] = "student";
-})(UserTypes = exports.UserTypes || (exports.UserTypes = {}));
-//# sourceMappingURL=users.js.map
